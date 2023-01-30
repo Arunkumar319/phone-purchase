ba5e875351243ce0e3ee37c11e8cbdcf9a3d82a1
Arunkumar319 <98182197+Arunkumar319@users.noreply.github.com>
2023-01-30 23:14:39 +0500

    Add files via upload

---
+++ PhonePurchase/src/main/java/ca/sheridancollege/phonepurchase/PhonePurchaseApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.phonepurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonePurchaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonePurchaseApplication.class, args);
+    }
+
+}
---
+++ PhonePurchase/src/test/java/ca/sheridancollege/phonepurchase/PhonePurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.phonepurchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonePurchaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
